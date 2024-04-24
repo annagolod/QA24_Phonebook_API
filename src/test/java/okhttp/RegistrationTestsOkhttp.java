9dbd06141d0d8baa0ef973df638bfa0990efc427
annagolod <anna.golod@gmail.com>
2024-04-24 18:39:12 +0300

    Added RegistrationTestsOkhttp

---
+++ src/test/java/okhttp/RegistrationTestsOkhttp.java
@@ -0,0 +1,97 @@
+package okhttp;
+
+import com.google.gson.Gson;
+import dto.AuthRequestDTO;
+import dto.AuthResponseDTO;
+import dto.ErrorDTO;
+import okhttp3.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class RegistrationTestsOkhttp {
+    Gson gson = new Gson();
+    public static final MediaType JSON = MediaType.get("application/json;charset=utf-8");
+    OkHttpClient client = new OkHttpClient();
+
+    @Test
+    public void registrationSuccess() throws IOException {
+        AuthRequestDTO auth = AuthRequestDTO.builder()
+                .username("totam@gmail.com")
+                .password("Totam54321#")
+                .build();
+        RequestBody body = RequestBody.create(gson.toJson(auth), JSON);
+        Request request = new Request.Builder()
+                .url("https://contactapp-telran-backend.herokuapp.com/v1/user/registration/usernamepassword")
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+
+        Assert.assertTrue(response.isSuccessful());
+        Assert.assertEquals(response.code(), 200);
+
+        AuthResponseDTO responseDTO = gson.fromJson(response.body().string(), AuthResponseDTO.class);
+        String token = responseDTO.getToken();
+
+    }
+    @Test
+    public void registrationWrongEmail() throws IOException {
+
+        AuthRequestDTO auth = AuthRequestDTO.builder()
+                .username("totamgmail.com")
+                .password("Totam54321#")
+                .build();
+        RequestBody body = RequestBody.create(gson.toJson(auth), JSON);
+        Request request = new Request.Builder()
+                .url("https://contactapp-telran-backend.herokuapp.com/v1/user/registration/usernamepassword")
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+
+        Assert.assertFalse(response.isSuccessful());
+        Assert.assertEquals(response.code(), 400);
+
+        ErrorDTO errorDTO = gson.fromJson(response.body().string(), ErrorDTO.class);
+
+    }
+
+    @Test
+    public void registrationWrongPassword() throws IOException {
+
+        AuthRequestDTO auth = AuthRequestDTO.builder()
+                .username("totam@gmail.com")
+                .password("tam54321")
+                .build();
+        RequestBody body = RequestBody.create(gson.toJson(auth), JSON);
+        Request request = new Request.Builder()
+                .url("https://contactapp-telran-backend.herokuapp.com/v1/user/registration/usernamepassword")
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+
+        Assert.assertFalse(response.isSuccessful());
+        Assert.assertEquals(response.code(), 400);
+
+        ErrorDTO errorDTO = gson.fromJson(response.body().string(), ErrorDTO.class);
+    }
+    @Test
+    public void registrationExistsUser() throws IOException {
+
+        AuthRequestDTO auth = AuthRequestDTO.builder()
+                .username("tretam0810@gmail.com")
+                .password("Phone54321#")
+                .build();
+        RequestBody body = RequestBody.create(gson.toJson(auth), JSON);
+        Request request = new Request.Builder()
+                .url("https://contactapp-telran-backend.herokuapp.com/v1/user/registration/usernamepassword")
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+
+        Assert.assertFalse(response.isSuccessful());
+        Assert.assertEquals(response.code(), 409);
+
+        ErrorDTO errorDTO = gson.fromJson(response.body().string(), ErrorDTO.class);
+    }
+}
